8b96d6729c19dec7f78fe735d22cf30d01bae131
sabbircse3 <sabbir.cse3@gmail.com>
2023-02-15 12:37:55 +0600

    second commit

---
+++ array-loop.js
@@ -0,0 +1,7 @@
+// target: display every elements of an array
+var numbers = [45, 87, 89, 56, 32, 51, 25];
+for (i = 0; i < 7; i++) { 
+    // console.log(i);
+    var number = numbers[i];
+    console.log(number);
+}
--- for.js
+++ for.js
@@ -1,1 +1,15 @@
-var roastGiven = 0;
+/* 
+ var roastGiven = 0;
+while (roastGiven < 7) { 
+    console.log('Roast Den, Please!');
+    console.log(roastGiven);
+    roastGiven++;
+}
+
+ */
+
+// for loop
+for (var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Roast Den, Please!');
+    // console.log(roastGiven);
+}
